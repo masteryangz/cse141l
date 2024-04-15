7b1cc1aa0342f86f20e12237a2b3849e7aa5d590
masteryangz <yangzuo2001@gmail.com>
2024-04-15 15:07:07 -0700

    complete arithmaticDifference

--- ArithmaticDifference.py
+++ ArithmaticDifference.py
@@ -3,8 +3,15 @@ def arithmaticDifference(n):
     min = 16
     for i in range(len(n)):
         for j in range(i+1, len(n)):
-            while()
+            setBits = n[i] + (~n[j] + 1)
+            if(setBits<0):
+                setBits = -setBits
+            if(setBits>max):
+                max = setBits
+            elif(setBits<min):
+                min = setBits 
     return max, min
+
 if __name__=='__main__':
-    n = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    n = [0, -2, 4, -1]
     print(arithmaticDifference(n))
