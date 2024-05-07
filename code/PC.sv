56819a149dfddd7261613ccc4ceeb649824f4e5a
masteryangz <yangzuo2001@gmail.com>
2024-05-07 14:11:52 -0700

    modified PC

--- code/PC.sv
+++ code/PC.sv
@@ -4,16 +4,19 @@
 module PC #(parameter D=9)(
   input reset,					// synchronous reset
         clk,
-		    reljump_en,             // rel. jump enable
-        absjump_en,				// abs. jump enable
-  input       [D-1:0] target,	// how far/where to jump
-  output logic[D-1:0] prog_ctr
+		    //reljump_en,             // rel. jump enable
+        //absjump_en,				// abs. jump enable
+  input       [D-1:0] prog_ctr_in,	// how far/where to jump
+  output logic[D-1:0] prog_ctr_out
 );
-
+  logic start;
+  logic branch;
+  logic taken;
+  logic [D-1:0]target;
+  logic [D-1:0]prog_ctr;
+  nextPC np(.*)
   always_ff @(posedge clk)
     if(reset) prog_ctr <= '0;
-	  else if(reljump_en) prog_ctr <= prog_ctr + target;
-    else if(absjump_en) prog_ctr <= target;
-	  else prog_ctr <= prog_ctr + 'b1;
+	  else prog_ctr_out <= prog_ctr_in;
 
 endmodule
