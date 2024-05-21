33fe821eba29bc733db6bda3eb5a7a9b783aeb37
vanvansan <103155715+vanvansan@users.noreply.github.com>
2024-05-21 00:55:37 -0700

    multiply done
    multiply pesudocode + restored dev doc

---
+++ DevelopmentDoc.md
@@ -0,0 +1,43 @@
+## 2024.5.21  
+
+## TODO:
+  1. need to test run machine codes
+  2. ALU needs carry out
+
+
+ISA size:
+ - 9 bits  
+
+
+Register:
+- size: 1 byte
+- total amount: 8 
+- namespace: 3 bits
+
+Instructions:
+[link to ISA pdf](https://drive.google.com/file/d/13Y4vb4rbdHncJQRD8_gCckEcnlmZvwQp/view?usp=sharing)
+- total amount: 8 
+- namespace: 3 bits
+- insns:
+  - ld: 000
+    -  load an intermediate to a reg
+    -  e.g. `ld R3 111⇔ 000_011_111`
+  - rst: 001
+    - by default left shift
+    -  e.g. `rst R0 101  ⇔ 001_000_101`
+  - add: 010
+    - e.g. `add R0, R1 ⇔ 010_000_001`
+  - pos: 011
+    - e.g. `pos R0 R1 ⇔ 011_000_001`
+  - xor: 100
+    -  bitwise xor
+    -  e.g. `xor R0 R1 ⇔ 100_000_001`
+  - jmp: 101
+    -  e.g. `beq R0 001 ⇔ 101_000_001`
+  - lw: 110
+    -  load from memory at reg
+    -  e.g `lw R0 R1 ⇔ 110_000_001`
+  - sw: 111
+    -  store to memory at reg
+    -  e.g. `sw R0 R1 ⇔ 111_000_001`
+ 
---
+++ multiply.py
@@ -0,0 +1,38 @@
+# let n be an array of length 64 with 8-bit elements
+# do multiply in an order of b2*a2, b2*a1, b1*a2, b1*a1
+# a1 is multiplier
+def doublePrecisionMult(array):
+    for pairOffset in range(16): # 16 pairs of multiplicant and multipliers
+        signExtend_subtract = 0b00
+        a1 = array[pairOffset + 0]
+        a2 = array[pairOffset + 1]
+        b1 = array[pairOffset + 2]
+        b2 = array[pairOffset + 3]
+        
+        f1,f2 = multiply(b2,a2,signExtend_subtract)
+        signExtend_subtract += 1
+        g1,g2 = multiply(b2,a1, signExtend_subtract)
+        signExtend_subtract += 1
+        h1,h2 = multiply(b1, a2, signExtend_subtract)
+        signExtend_subtract += 1
+        i1,i2 = multiply(b1, a1, signExtend_subtract)
+        array[64 + pairOffset + 3] = f2
+        array[64 + pairOffset + 2] = f1 + g2 + h2
+        array[64 + pairOffset + 1] = h1 + g1 + i2 + overflow
+        array[64 + pairOffset + 0] = i1 + Overflow
+        
+
+def multiply(multiplicant, multiplier, signExtend_subtract):
+    accumulator = 0
+    for i in range(8): # repeat 8 times
+        if (multiplier[0] == 1):
+            if(signExtend_subtract & 0b01): #if subtract bit is on
+                accumulator -= multiplicant
+            else:
+                accumulator += multiplicant
+        multiplier >> 1
+        multiplier[7] = multiplicant[0]
+        multiplicant >> 1
+        if(signExtend_subtract & 0b10): #if signExtend bit is on
+            multiplicant[7] = 1
+    return multiplicant, multiplier
