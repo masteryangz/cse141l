9defc27c08145da4c79462e06f16f08909e00ccb
masteryangz <yangzuo2001@gmail.com>
2024-06-13 20:14:47 -0700

    6/13

--- code/PC_LUT.sv
+++ code/PC_LUT.sv
@@ -4,13 +4,13 @@ module PC_LUT #(parameter D=12)(
 
   always_comb case(how_high)
     0: target = 2;  
-    1: target = 5;//4;
+    1: target = 8;//6;
 	2: target = 22;//k loop
 	3: target = -26;//back to k loop
-	4: target = 171;//130j loop
-	5: target = -173;//-132back to j loop
-	6: target = 203;//162i loop
-	7: target = -209;//-168back to i loop
+	4: target = 184;//171j loop
+	5: target = -186;//-173back to j loop
+	6: target = 216;//203i loop
+	7: target = -222;//-209back to i loop
 	default: target = 'b0;  // hold PC  
   endcase
 
